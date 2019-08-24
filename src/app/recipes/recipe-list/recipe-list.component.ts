5e1d3ef46b67039602a9c98dd06b62488e20b3ae
Sarvesh Kujur <Sarvesh.Kujur@betsol.com>
2019-08-24 13:39:41 +0530

    Added a Recipe model to create Recipe list item and created recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Rhubarb-Crisp_EXPS_CIMZ19_4530_C01_09_6b-696x696.jpg'),
+    new Recipe('A test Recipe', 'This is simply a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Rhubarb-Crisp_EXPS_CIMZ19_4530_C01_09_6b-696x696.jpg')
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
