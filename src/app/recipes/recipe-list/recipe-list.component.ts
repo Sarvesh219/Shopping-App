9d4db9600f4757b35384560756334393a7300595
Sarvesh Kujur <Sarvesh.Kujur@betsol.com>
2019-09-01 20:04:23 +0530

    Implemented the Recipe Details section and it's working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Rhubarb-Crisp_EXPS_CIMZ19_4530_C01_09_6b-696x696.jpg'),
-    new Recipe('A test Recipe', 'This is simply a test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Rhubarb-Crisp_EXPS_CIMZ19_4530_C01_09_6b-696x696.jpg')
+    new Recipe('Another test Recipe', 'This is simply another test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Rhubarb-Crisp_EXPS_CIMZ19_4530_C01_09_6b-696x696.jpg')
   ];
 
   constructor() {}
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
